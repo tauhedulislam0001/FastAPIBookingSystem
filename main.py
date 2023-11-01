bdbe808039bc1dacb9a1d7b529534533f5c7b22a
tauhedulislam0001 <tauhedulislam0001@gmail.com>
2023-11-01 14:24:33 +0600

    fixed mapped column

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, HTTPException, Depends,Request
+from fastapi import FastAPI, HTTPException, Depends, Request
 from pydantic import BaseModel
-from typing import Annotated
 import models
-from database import engine, SessionLocal,Base
+from database import engine, SessionLocal, Base
 from sqlalchemy.orm import Session
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import mapped_column 
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -16,75 +19,77 @@ app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
 
 class Customers(Base):
     __tablename__ = "customers"
-    
-    
-    user_type : int
-    name : str 
-    email : str 
-    password : str 
-    access_token : str 
-    refresh_token : str 
-    image : str 
-    status : str 
-    created_at : str 
-    
-    
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_type = mapped_column(Integer)
+    name = mapped_column(String)
+    email = mapped_column(String)
+    password = mapped_column(String)
+    access_token = mapped_column(String)
+    refresh_token = mapped_column(String)
+    image = mapped_column(String)
+    status = mapped_column(String)
+    created_at = mapped_column(String)
+
+
 class Drivers(Base):
     __tablename__ = "drivers"
 
+    id = Column(Integer, primary_key=True, index=True)
+    user_type = mapped_column(Integer)
+    name = mapped_column(String)
+    email = mapped_column(String)
+    password = mapped_column(String)
+    access_token = mapped_column(String)
+    refresh_token = mapped_column(String)
+    image = mapped_column(String)
+    status = mapped_column(Integer)
+    created_at = mapped_column(String)
 
-    user_type : int 
-    name : str 
-    email : str 
-    password : str 
-    access_token : str 
-    refresh_token : str 
-    image : str 
-    status : int 
-    created_at : str 
-    
 
 class Trips(Base):
     __tablename__ = "trips"
 
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = mapped_column(Integer)
+    driver_id = mapped_column(Integer)
+    car_name = mapped_column(String)
+    location = mapped_column(String)
+    fare = mapped_column(String)
+    status = mapped_column(Integer)
+    created_at = mapped_column(String)
 
-    user_id : int 
-    driver_id : int 
-    car_name : str 
-    location : str 
-    fare : str 
-    status : int 
-    created_at : str 
-    
-    
 
 class Bids(Base):
     __tablename__ = "bids"
-    
-    id : int 
-    trip_id : int 
-    driver_id : int 
-    amount : int 
-    status : int 
-    created_at : int 
-    
+
+    id = Column(Integer, primary_key=True, index=True)
+    trip_id = mapped_column(Integer)
+    driver_id = mapped_column(Integer)
+    amount = mapped_column(Integer)
+    status = mapped_column(Integer)
+    created_at = mapped_column(Integer)
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
-
-db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.route("/", methods=["GET", "POST"])
+db_dependency = Depends(get_db)
+
+
+@app.get("/")
 async def read_root(request: Request):
     error_message = request.query_params.get("error")
     success_message = request.query_params.get("success")
-    return templates.TemplateResponse("index.html", {"request": request, "error": error_message, "success":success_message})
+    return templates.TemplateResponse("index.html", {"request": request, "error": error_message, "success": success_message})
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
