99e727e23927095d9302f734dd45fdec7d548c0a
Mehedi Hasan <mehedinaeem66@gmail.com>
2023-11-06 13:08:04 +0600

    ip add

--- routes/drivers.py
+++ routes/drivers.py
@@ -63,9 +63,10 @@ async def trips_get(id: int,request: Request,db:Annotated[Session, Depends(get_d
     try:
         user = await decode_token(token, db)
         bids = db.query(models.Bids).join(models.Drivers).join(models.Trips).filter(models.Bids.trip_id == id).all()
+        bidsss = db.query(models.Bids).filter(models.Bids.trip_id == id).all()
         bid_data = [(bid.id, bid.amount, bid.driver.name) for bid in bids]
         trip_data = [(bid.id, bid.amount, bid.trip.fare) for bid in bids]
-        return {'bids': bids, 'user': user}
+        return {'bids': bids, 'user': user,'bidsss':bidsss}
     except TokenDecodeError as e:
         return RedirectResponse("/?error=You+are+not+authorized",302)
 
