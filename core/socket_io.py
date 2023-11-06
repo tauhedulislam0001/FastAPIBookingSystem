339b59fd1ade250f432efd971e70d3e0fb990f20
Mehedi Hasan <mehedinaeem66@gmail.com>
2023-11-06 13:26:44 +0600

    ip add

--- core/socket_io.py
+++ core/socket_io.py
@@ -2,7 +2,7 @@ import socketio
 
 # comment/edit line 5 if you don't want use redis or using other message queue
 # see https://python-socketio.readthedocs.io/en/latest/server.html#using-a-message-queue
-mgr = socketio.AsyncRedisManager(url="redis://localhost:6379/0") 
+mgr = socketio.AsyncRedisManager(url="redis://143.110.250.65:6379/0") 
 sio = socketio.AsyncServer(
     async_mode="asgi", cors_allowed_origins="*", client_manager=mgr
 )
