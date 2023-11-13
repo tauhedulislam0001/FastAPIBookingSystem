9ec2857554c182736e3b71412394d58c7581705d
Mehedi Hasan <mehedinaeem66@gmail.com>
2023-11-13 12:30:20 +0600

    security add

--- core/helper.py
+++ core/helper.py
@@ -1,23 +1,36 @@
 from datetime import datetime
 import os
 import uuid
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
 
 
 async def insert_image(image, dir):
-    currentDate = datetime.today()
-    formatted_date = currentDate.strftime("%Y-%m-%d_%H-%M-%S-%f")
-    random_filename = str(uuid.uuid4())
+    try:
+        currentDate = datetime.today()
+        formatted_date = currentDate.strftime("%Y-%m-%d_%H-%M-%S-%f")
+        random_filename = str(uuid.uuid4())
+        # Get the original file extension from the image filename
+        _, file_extension = os.path.splitext(image.filename)
 
-    filename = f'{random_filename}_{image.filename}' 
+        # Check if the file extension is allowed (jpg, png, jpeg, webp)
+        allowed_extensions = {'.jpg', '.jpeg', '.png', '.webp'}
+        if file_extension.lower() not in allowed_extensions:
+            raise ValueError("Invalid file type. Only jpg, png, jpeg, and webp are allowed.")
+
+        filename = f'{random_filename}_{image.filename}' 
 
 
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
-    contents = await image.read()
-    with open(f"{dir}{filename}", "wb") as f:
-        f.write(contents)
-    return filename
+        contents = await image.read()
+        with open(f"{dir}{filename}", "wb") as f:
+            f.write(contents)
+        return filename
+    except ValueError as e:
+        error_message = str(e)
+        return None
+
 
 
 async def get_user_by_email(email: str, db, models):
