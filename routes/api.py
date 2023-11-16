5f0cdaa58f5c5cec94e2daeebb38e7c1bb13846e
Mehedi Hasan <mehedinaeem66@gmail.com>
2023-11-16 13:04:12 +0600

    api driver

--- routes/api.py
+++ routes/api.py
@@ -80,3 +80,54 @@ async def register(
     return JSONResponse(content={"success": "Customer Registration successfully"}, status_code=200)
 
 
+
+@api.post("/driver/register/submit")
+async def driver_register(
+        db: db_dependency,
+        name: str = Form(None),
+        email: str = Form(None),
+        password: str = Form(None),
+        confirm_password: str = Form(None),
+        image: UploadFile = File(None),
+):
+    if  name is None :
+        return JSONResponse(content={"error": "Name is require"}, status_code=500)
+    if  email is None:
+        return JSONResponse(content={"error": "Email is required"}, status_code=500)
+    if  password is None:
+        return JSONResponse(content={"error": "Password is require"}, status_code=500)
+    if  confirm_password is None:
+        return JSONResponse(content={"error": "Confirm password is required"}, status_code=500)
+    if  image is None:
+        return JSONResponse(content={"error": "Image is required"}, status_code=500)
+    if password != confirm_password:
+        return JSONResponse(content={"error": "Passwords do not match"}, status_code=500)
+    
+    existing_driver = await get_user_by_email(email, db, models.Drivers)
+    existing_customer = await get_user_by_email(email, db, models.Customers)
+    existing_admin = await get_user_by_email(email, db, models.Admins)
+    if existing_driver:
+        return JSONResponse(content={"error": "Email already exists"}, status_code=500)
+    elif existing_customer:
+        return JSONResponse(content={"error": "Email already exists"}, status_code=500)
+    elif existing_admin:
+        return JSONResponse(content={"error": "Email already exists"}, status_code=500)
+    # hashed_password = pwd_context.hash(password)
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+    dir = "templates/assets/upload/profile/"
+    filename = await insert_image(image, dir)
+    if filename is None:
+        return JSONResponse(content={"error": "Invalid file type. Only jpg, png, jpeg, and webp are allowed"}, status_code=500)
+    print(f"file:{filename}")
+    register = models.Drivers(
+        name=name,
+        email=email,
+        password=hashed_password,
+        image=filename,
+    )
+    db.add(register)
+    db.commit()
+    db.refresh(register)
+    return JSONResponse(content={"success": "Driver Registration successfully"}, status_code=200)
+
